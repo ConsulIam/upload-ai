fb1001ecdf411a4846f16ad5fd664d0932f0f589
consuliam <consuliam@gmail.com>
2023-09-13 22:37:55 -0300

    Backend finalized including integration with OpenAI

---
+++ api/src/routes/generate-ai-completion.ts
@@ -0,0 +1,38 @@
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
+
+export async function generateAICompletionRoute(app: FastifyInstance) {
+    app.post('/ai/complete', async (request, reply) => {
+        const bodySchema = z.object({
+            videoId : z.string().uuid(),
+            template: z.string(),
+            temperature: z.number().min(0).max(1).default(0.3),
+        })
+
+        const { videoId, template, temperature } = bodySchema.parse(request.body)
+
+        const video = await prisma.video.findFirstOrThrow({
+            where: {
+                id: videoId,
+            }
+        })
+
+        if (!video.transcription) {
+            return reply.status(400).send({ error: "Video transcription not yet generated."})
+        }
+
+        const promptMessage = template.replace('{transcription}', video.transcription)
+
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            temperature,
+            messages: [
+                { role: 'user', content: promptMessage },
+            ],
+        })
+
+        return response
+    })
+}
---
+++ api/src/server.ts
@@ -0,0 +1,23 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors"
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video"
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+
+const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*',
+})
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
+
+app.listen({
+    port: 11337
+}).then(() => {
+    console.log("HTTP server running at port 11337!")
+})
